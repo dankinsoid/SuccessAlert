2d9506f0d7bb2228ea4d537ab336ad3d04f5bb63
AppDeveloper428 <voidilov@gmail.com>
2018-04-14 12:40:16 +0300

    Initial Commit

---
+++ SuccessAlert/SuccessAlert.h
@@ -0,0 +1,19 @@
+//
+//  SuccessAlert.h
+//  SuccessAlert
+//
+//  Created by Данил Войдилов on 14.04.2018.
+//  Copyright © 2018 Данил Войдилов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SuccessAlert.
+FOUNDATION_EXPORT double SuccessAlertVersionNumber;
+
+//! Project version string for SuccessAlert.
+FOUNDATION_EXPORT const unsigned char SuccessAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuccessAlert/PublicHeader.h>
+
+
---
+++ SuccessAlertTests/SuccessAlertTests.swift
@@ -0,0 +1,36 @@
+//
+//  SuccessAlertTests.swift
+//  SuccessAlertTests
+//
+//  Created by Данил Войдилов on 14.04.2018.
+//  Copyright © 2018 Данил Войдилов. All rights reserved.
+//
+
+import XCTest
+@testable import SuccessAlert
+
+class SuccessAlertTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
