81112ec75b7868ddb371e8f2e42fc1f4562a9d0e
Daniil <voidilov@gmaill.com>
2018-04-16 09:43:39 +0300

    no message

--- SuccessAlert/UIWindow_Extension.swift
+++ SuccessAlert/UIWindow_Extension.swift
@@ -10,26 +10,26 @@ import UIKit
 
 extension UIWindow {
     
-    open func showSuccessAlert(title: String = "Выполнено", message: String? = nil, view: AnimatedView? = nil, color: UIColor = #colorLiteral(red: 0.3489781618, green: 0.3490435183, blue: 0.3489740491, alpha: 1), textColor: UIColor = #colorLiteral(red: 0.3489781618, green: 0.3490435183, blue: 0.3489740491, alpha: 1), blur: UIBlurEffectStyle = .extraLight, complition: (() -> ())? = nil) {
+    open func showSuccessAlert(title: String = "Выполнено", message: String? = nil, view: AnimatedView? = nil, color: UIColor = #colorLiteral(red: 0.3489781618, green: 0.3490435183, blue: 0.3489740491, alpha: 1), textColor: UIColor = #colorLiteral(red: 0.3489781618, green: 0.3490435183, blue: 0.3489740491, alpha: 1), blur: UIBlurEffectStyle = .extraLight, completion: (() -> ())? = nil) {
         let view = SuccessAlert(frame: bounds, title: title, message: message, view: view, color: color, textColor: textColor, blur: blur)
         endEditing(true)
         addSubview(view)
-        appearingAnimation(view, complition: complition)
+        appearingAnimation(view, completion: completion)
     }
     
-    open func showSuccessAlert(title: String = "Выполнено", message: String? = nil, image: UIImage, color: UIColor = #colorLiteral(red: 0.3489781618, green: 0.3490435183, blue: 0.3489740491, alpha: 1), textColor: UIColor = #colorLiteral(red: 0.3489781618, green: 0.3490435183, blue: 0.3489740491, alpha: 1), blur: UIBlurEffectStyle = .extraLight, complition: (() -> ())? = nil) {
-        showSuccessAlert(title: title, message: message, view: UIImageView(image: image.withRenderingMode(.alwaysTemplate)), color: color, textColor: textColor, blur: blur, complition: complition)
+    open func showSuccessAlert(title: String = "Выполнено", message: String? = nil, image: UIImage, color: UIColor = #colorLiteral(red: 0.3489781618, green: 0.3490435183, blue: 0.3489740491, alpha: 1), textColor: UIColor = #colorLiteral(red: 0.3489781618, green: 0.3490435183, blue: 0.3489740491, alpha: 1), blur: UIBlurEffectStyle = .extraLight, completion: (() -> ())? = nil) {
+        showSuccessAlert(title: title, message: message, view: UIImageView(image: image.withRenderingMode(.alwaysTemplate)), color: color, textColor: textColor, blur: blur, completion: completion)
     }
     
-    static open func showSuccessAlert(title: String = "Выполнено", message: String? = nil, image: UIImage, color: UIColor = #colorLiteral(red: 0.3489781618, green: 0.3490435183, blue: 0.3489740491, alpha: 1), textColor: UIColor = #colorLiteral(red: 0.3489781618, green: 0.3490435183, blue: 0.3489740491, alpha: 1), blur: UIBlurEffectStyle = .extraLight, complition: (() -> ())? = nil) {
-        UIApplication.shared.keyWindow?.showSuccessAlert(title: title, message: message, view: UIImageView(image: image.withRenderingMode(.alwaysTemplate)), color: color, textColor: textColor, blur: blur, complition: complition)
+    static open func showSuccessAlert(title: String = "Выполнено", message: String? = nil, image: UIImage, color: UIColor = #colorLiteral(red: 0.3489781618, green: 0.3490435183, blue: 0.3489740491, alpha: 1), textColor: UIColor = #colorLiteral(red: 0.3489781618, green: 0.3490435183, blue: 0.3489740491, alpha: 1), blur: UIBlurEffectStyle = .extraLight, completion: (() -> ())? = nil) {
+        UIApplication.shared.keyWindow?.showSuccessAlert(title: title, message: message, view: UIImageView(image: image.withRenderingMode(.alwaysTemplate)), color: color, textColor: textColor, blur: blur, completion: completion)
     }
     
-    static open func showSuccessAlert(title: String = "Выполнено", message: String? = nil, view: AnimatedView? = nil, color: UIColor = #colorLiteral(red: 0.3489781618, green: 0.3490435183, blue: 0.3489740491, alpha: 1), textColor: UIColor = #colorLiteral(red: 0.3489781618, green: 0.3490435183, blue: 0.3489740491, alpha: 1), blur: UIBlurEffectStyle = .extraLight, complition: (() -> ())? = nil) {
-        UIApplication.shared.keyWindow?.showSuccessAlert(title: title, message: message, view: view, color: color, textColor: textColor, blur: blur, complition: complition)
+    static open func showSuccessAlert(title: String = "Выполнено", message: String? = nil, view: AnimatedView? = nil, color: UIColor = #colorLiteral(red: 0.3489781618, green: 0.3490435183, blue: 0.3489740491, alpha: 1), textColor: UIColor = #colorLiteral(red: 0.3489781618, green: 0.3490435183, blue: 0.3489740491, alpha: 1), blur: UIBlurEffectStyle = .extraLight, completion: (() -> ())? = nil) {
+        UIApplication.shared.keyWindow?.showSuccessAlert(title: title, message: message, view: view, color: color, textColor: textColor, blur: blur, completion: completion)
     }
     
-    fileprivate func appearingAnimation(_ view: SuccessAlert, complition: (() -> ())? = nil) {
+    fileprivate func appearingAnimation(_ view: SuccessAlert, completion: (() -> ())? = nil) {
         view.checkMarkView.transform = CGAffineTransform(scaleX: 0.00001, y: 0.00001)
         view.checkMarkView.alpha = 0
         UIView.animate(withDuration: 0.3, animations: {
@@ -37,18 +37,18 @@ extension UIWindow {
             view.checkMarkView.transform = CGAffineTransform(scaleX: 1, y: 1)
         }) { (_) in
             view.drawAnimate({
-                self.disappearingAnimation(view, complition: complition)
+                self.disappearingAnimation(view, completion: completion)
             })
         }
     }
     
-    fileprivate func disappearingAnimation(_ view: SuccessAlert, complition: (() -> ())? = nil) {
+    fileprivate func disappearingAnimation(_ view: SuccessAlert, completion: (() -> ())? = nil) {
         UIView.animate(withDuration: 0.3, delay: 0.4, options: [], animations: {
             view.checkMarkView.transform = CGAffineTransform(scaleX: 0.00001, y: 0.00001)
             view.checkMarkView.alpha = 0
         }) { (_) in
             view.removeFromSuperview()
-            complition?()
+            completion?()
         }
     }
 }
